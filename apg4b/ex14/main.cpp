71a372574fdabc3d1c119c18bf53fa24740e8efb
ncrpy <61719652+ncrpy@users.noreply.github.com>
2021-02-19 17:47:49 +0900

    APG4b -ex15

--- apg4b/ex14/main.cpp
+++ apg4b/ex14/main.cpp
@@ -3,6 +3,12 @@
 using namespace std;
 
 int main() {
-    
+    vector<int> v(3);
+    for (int i = 0; i < 3; i++) {
+        cin >> v.at(i);
+    }
+    int max = *max_element(v.begin(), v.end());
+    int min = *min_element(v.begin(), v.end());
+    cout << max - min << endl;
     return 0;
 }
